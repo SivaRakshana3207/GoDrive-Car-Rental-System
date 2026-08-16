d89cf2742f3ac6372cf1f6ecd55607750a087955
Siva Rakshana <sivarakshana182@gmail.com>
2026-08-16 11:30:51 +0530

    Remove backend image upload and use common React image

--- backend/car-rental/src/main/java/com/example/car_rental/controller/CarController.java
+++ backend/car-rental/src/main/java/com/example/car_rental/controller/CarController.java
@@ -3,12 +3,9 @@ package com.example.car_rental.controller;
 import com.example.car_rental.entity.Car;
 import com.example.car_rental.service.CarService;
 
-import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -23,24 +20,8 @@ public class CarController {
         return carService.getAllCars();
     }
 
-    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public Car addCar(
-            @RequestParam("carName") String carName,
-            @RequestParam("seats") int seats,
-            @RequestParam("place") String place,
-            @RequestParam("price") int price,
-            @RequestParam("booked") boolean booked,
-            @RequestParam("image") MultipartFile imageFile
-    ) throws IOException {
-
-        Car car = new Car();
-        car.setCarName(carName);
-        car.setSeats(seats);
-        car.setPlace(place);
-        car.setPrice(price);
-        car.setBooked(booked);
-
-        return carService.addCar(car, imageFile);
+    @PostMapping
+    public Car addCar(@RequestBody Car car) {
+        return carService.addCar(car);
     }
-
 }
--- backend/car-rental/src/main/java/com/example/car_rental/service/CarService.java
+++ backend/car-rental/src/main/java/com/example/car_rental/service/CarService.java
@@ -3,23 +3,15 @@ package com.example.car_rental.service;
 import com.example.car_rental.entity.Car;
 import com.example.car_rental.repository.CarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.File;
-import java.io.IOException;
 import java.util.List;
-import java.util.UUID;
 
 @Service
 public class CarService {
 
     @Autowired
     private CarRepository carRepository;
-
-    @Value("${upload.dir}")
-    private String uploadDir;
 
     public List<Car> getAllCars() {
         return carRepository.findAll();
@@ -29,26 +21,7 @@ public class CarService {
         return carRepository.findById(id).orElse(null);
     }
 
-    public Car addCar(Car car, MultipartFile imageFile) throws IOException {
-
-        File dir = new File(uploadDir);
-
-        if (!dir.exists()) {
-            dir.mkdirs();
-        }
-
-        String originalFileName = imageFile.getOriginalFilename();
-
-        String fileName = UUID.randomUUID() + "_" + originalFileName;
-
-        File destination = new File(dir, fileName);
-
-        System.out.println("Saving image to: " + destination.getAbsolutePath());
-
-        imageFile.transferTo(destination);
-
-        car.setImage(fileName);
-
+    public Car addCar(Car car) {
         return carRepository.save(car);
     }
 }
